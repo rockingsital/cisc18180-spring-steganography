5b0f276119bbd54fe3b43b958221e94f8739da98
stephen272 <stephen272@d560dd79-17e9-b207-13ba-76aafc30dfa7>
2011-05-11 17:44:55 +0000

    Android App!

---
+++ src/com/example/HelloAndroid/PictureTab.java
@@ -0,0 +1,58 @@
+package com.example.HelloAndroid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class PictureTab extends Activity{
+	TextView textTargetUri1;
+	TextView textTargetUri2;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.picturetab);
+
+		Button buttonLoadImage = (Button)findViewById(R.id.button1);
+		textTargetUri1 = (TextView)findViewById(R.id.textView3);
+		Button buttonLoadImage2 = (Button)findViewById(R.id.button2);
+		textTargetUri2 = (TextView)findViewById(R.id.textView4);
+
+		buttonLoadImage.setOnClickListener(new Button.OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(Intent.ACTION_PICK,
+						android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+				startActivityForResult(intent, 0);
+			}});
+
+		buttonLoadImage2.setOnClickListener(new Button.OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(Intent.ACTION_PICK,
+						android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+				startActivityForResult(intent, 1);
+			}});
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+		Uri targetUri = data.getData();
+		if (resultCode == RESULT_OK){
+			if (requestCode == 0)
+				textTargetUri1.setText(targetUri.toString());
+			else if (requestCode ==1)
+				textTargetUri2.setText(targetUri.toString());
+		}
+	}
+}
